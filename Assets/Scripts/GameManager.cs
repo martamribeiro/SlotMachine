7287c9bc88906de97828c673930fdde264360614
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2024-02-26 20:33:03 +0000

    Improved UI and added combinations.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     Sprite[] symbols;
 
-    public GameObject slotOne, slotTwo, slotThree, handleButton, handle;
+    public GameObject slotOne, slotTwo, slotThree, spinButton;
 
     public TMP_Text creditsText, betText, message, additionalInfo;
 
@@ -39,7 +39,6 @@ public class GameManager : MonoBehaviour
         image.color = color;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (gameStarted)
@@ -48,13 +47,7 @@ public class GameManager : MonoBehaviour
 
             gameStarted = false;
 
-            // Get the Animator component of the handle game object
-            Animator handleAnimator = handle.GetComponent<Animator>();
-
-            // Set a trigger parameter to stop the animation
-            handleAnimator.SetTrigger("Stop");
-
-            handleButton.SetActive(true);
+            spinButton.SetActive(true);
         }
     }
 
@@ -88,15 +81,7 @@ public class GameManager : MonoBehaviour
 
         if (bet > 0) {
 
-            handleButton.SetActive(false);
-
-            //play animation
-
-            // Get the Animator component of the handle game object
-            Animator handleAnimator = handle.GetComponent<Animator>();
-
-            // Play the animation
-            handleAnimator.SetTrigger("Start");
+            spinButton.SetActive(false);
 
             StartCoroutine(SpinSlotMachine());
         }
@@ -104,9 +89,6 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpinSlotMachine()
     {
-        // Wait for 1 second before starting to spin
-        yield return new WaitForSeconds(1f);
-
         float spinDuration = 3f; // Duration for spinning the slots
         float elapsedTime = 0f;
         float symbolChangeInterval = 0.2f; // Delay between each symbol change
@@ -156,32 +138,144 @@ public class GameManager : MonoBehaviour
 
         if (IsThreeOfAKind())
         {
-            // Three of a kind combination, high payout
-            payout = bet*3;
+            // Three of a kind combination
+            payout = bet * 20;
             message.text = "Three of a kind!";
-            message.color = new Color(253f / 255f, 208f / 255f, 65f / 255f);
-            additionalInfo.text = "+ bet x3";
-            additionalInfo.color = new Color(253f / 255f, 208f / 255f, 65f / 255f);
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x20";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is three of a kind");
         }
         else if (IsTwoOfAKind())
         {
-            // Two of a kind combination, medium payout
-            payout = bet*2;
+            // Two of a kind combination
+            payout = bet * 5;
             message.text = "Two of a kind!";
-            message.color = new Color(253f / 255f, 208f / 255f, 65f / 255f);
-            additionalInfo.text = "+ bet x2";
-            additionalInfo.color = new Color(253f / 255f, 208f / 255f, 65f / 255f);
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x5";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is two of a kind");
+            if (IsOneSeven())
+            {
+                // Two of a kind + One 7 combination
+                payout = bet * 15;
+                message.text = "Any two + One 7!";
+                message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                additionalInfo.text = "+ bet x15";
+                additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                Debug.Log("Inside is two of a kind + is one seven");
+            }
+            else if (IsOneBar())
+            {
+                // Two of a kind + One bar combination
+                payout = bet * 10;
+                message.text = "Any two + One Bar!";
+                message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                additionalInfo.text = "+ bet x10";
+                additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                Debug.Log("Inside is two of a kind + is one bar");
+            }
+        }
+        else if (IsThreeSeven())
+        {
+            // Three 7's combination
+            payout = bet * 100;
+            message.text = "Three 7's!";
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x100";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is three seven");
+        }
+        else if (IsTwoSeven())
+        {
+            // Two 7's combination
+            payout = bet * 50;
+            message.text = "Two 7's!";
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x50";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is two seven");
+            if (IsOneBar())
+            {
+                // Two seven + One bar combination
+                payout = bet * 55;
+                message.text = "Two 7's + One Bar!";
+                message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                additionalInfo.text = "+ bet x55";
+                additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                Debug.Log("Inside is two seven and one bar");
+            }
+        }
+        else if (IsOneSeven())
+        {
+            // One 7 combination
+            payout = bet * 10;
+            message.text = "One 7!";
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x10";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is one seven");
+            if (IsTwoBar())
+            {
+                // one seven and two bars combination
+                payout = bet * 35;
+                message.text = "One 7 + Two Bars!";
+                message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                additionalInfo.text = "+ bet x35";
+                additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                Debug.Log("Inside is one seven and two bar");
+            }
+            else if (IsOneBar())
+            {
+                // one seven and one bar combination
+                payout = bet * 15;
+                message.text = "One 7 + One Bar!";
+                message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                additionalInfo.text = "+ bet x15";
+                additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+                Debug.Log("Inside is one seven and one bar");
+            }
+        }
+        else if (IsThreeBar())
+        {
+            // Three bars combination
+            payout = bet * 50;
+            message.text = "Three Bars!";
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x50";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is three bar");
+        }
+        else if (IsTwoBar())
+        {
+            // Two bars combination
+            payout = bet * 25;
+            message.text = "Two Bars!";
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x25";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is two bar");
+        }
+        else if (IsOneBar())
+        {
+            // One bar combination
+            payout = bet * 5;
+            message.text = "One Bar!";
+            message.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            additionalInfo.text = "+ bet x5";
+            additionalInfo.color = new Color(3f / 255f, 152f / 255f, 0f); // Green color for win
+            Debug.Log("Inside is one bar");
         }
         else
         {
-            credits = credits - bet;
+            // No winning combination, deduct bet
+            credits -= bet;
             message.text = "You lost!";
-            message.color = new Color(228f / 255f, 59f / 255f, 0f);
-            additionalInfo.text = "no prize";
-            additionalInfo.color = new Color(228f / 255f, 59f / 255f, 0f);
+            message.color = new Color(241f / 255f, 7f / 255f, 0f / 255f); // Red color for loss
+            additionalInfo.text = "No prize";
+            additionalInfo.color = new Color(241f / 255f, 7f / 255f, 0f / 255f); // Red color for loss
+            Debug.Log("Inside you lost");
         }
-        
-        //add more combinations
 
         credits += payout;
         ChangeCredits();
@@ -200,7 +294,8 @@ public class GameManager : MonoBehaviour
         var image2 = slotTwo.GetComponent<Image>();
         var image3 = slotThree.GetComponent<Image>();
 
-        return image1.sprite == image2.sprite && image2.sprite == image3.sprite;
+        return image1.sprite.name != "slot-symbol1" && image1.sprite.name != "slot-symbol4" &&
+           image1.sprite == image2.sprite && image2.sprite == image3.sprite;
     }
 
     bool IsTwoOfAKind()
@@ -210,9 +305,79 @@ public class GameManager : MonoBehaviour
         var image2 = slotTwo.GetComponent<Image>();
         var image3 = slotThree.GetComponent<Image>();
 
-        return (image1.sprite == image2.sprite && image1.sprite != image3.sprite) ||
-               (image1.sprite == image3.sprite && image1.sprite != image2.sprite) ||
-               (image2.sprite == image3.sprite && image2.sprite != image1.sprite);
+        return image1.sprite.name != "slot-symbol1" && image1.sprite.name != "slot-symbol4" &&
+            image2.sprite.name != "slot-symbol1" && image2.sprite.name != "slot-symbol4" &&
+           ((image1.sprite == image2.sprite && image1.sprite != image3.sprite) ||
+           (image1.sprite == image3.sprite && image1.sprite != image2.sprite) ||
+           (image2.sprite == image3.sprite && image2.sprite != image1.sprite));
+    }
+
+    bool IsThreeSeven()
+    {
+        // Three 7's combination
+        var image1 = slotOne.GetComponent<Image>();
+        var image2 = slotTwo.GetComponent<Image>();
+        var image3 = slotThree.GetComponent<Image>();
+
+        return image1.sprite.name == "slot-symbol1" && image2.sprite.name == "slot-symbol1" && image3.sprite.name == "slot-symbol1";
+    }
+
+    bool IsTwoSeven()
+    {
+        // Two 7's combination
+        var image1 = slotOne.GetComponent<Image>();
+        var image2 = slotTwo.GetComponent<Image>();
+        var image3 = slotThree.GetComponent<Image>();
+
+        return (image1.sprite.name == "slot-symbol1" && image2.sprite.name == "slot-symbol1" && image3.sprite.name != "slot-symbol1") ||
+               (image1.sprite.name == "slot-symbol1" && image2.sprite.name != "slot-symbol1" && image3.sprite.name == "slot-symbol1") ||
+               (image1.sprite.name != "slot-symbol1" && image2.sprite.name == "slot-symbol1" && image3.sprite.name == "slot-symbol1");
+    }
+
+    bool IsOneSeven()
+    {
+        // One 7's combination
+        var image1 = slotOne.GetComponent<Image>();
+        var image2 = slotTwo.GetComponent<Image>();
+        var image3 = slotThree.GetComponent<Image>();
+
+        return (image1.sprite.name == "slot-symbol1" && image2.sprite.name != "slot-symbol1" && image3.sprite.name != "slot-symbol1") ||
+               (image1.sprite.name != "slot-symbol1" && image2.sprite.name == "slot-symbol1" && image3.sprite.name != "slot-symbol1") ||
+               (image1.sprite.name != "slot-symbol1" && image2.sprite.name != "slot-symbol1" && image3.sprite.name == "slot-symbol1");
+    }
+
+    bool IsThreeBar()
+    {
+        // Three bars combination
+        var image1 = slotOne.GetComponent<Image>();
+        var image2 = slotTwo.GetComponent<Image>();
+        var image3 = slotThree.GetComponent<Image>();
+
+        return image1.sprite.name == "slot-symbol4" && image2.sprite.name == "slot-symbol4" && image3.sprite.name == "slot-symbol4";
+    }
+
+    bool IsTwoBar()
+    {
+        // Two bars combination
+        var image1 = slotOne.GetComponent<Image>();
+        var image2 = slotTwo.GetComponent<Image>();
+        var image3 = slotThree.GetComponent<Image>();
+
+        return (image1.sprite.name == "slot-symbol4" && image2.sprite.name == "slot-symbol4" && image3.sprite.name != "slot-symbol4") ||
+               (image1.sprite.name == "slot-symbol4" && image2.sprite.name != "slot-symbol4" && image3.sprite.name == "slot-symbol4") ||
+               (image1.sprite.name != "slot-symbol4" && image2.sprite.name == "slot-symbol4" && image3.sprite.name == "slot-symbol4");
+    }
+
+    bool IsOneBar()
+    {
+        // One bar combination
+        var image1 = slotOne.GetComponent<Image>();
+        var image2 = slotTwo.GetComponent<Image>();
+        var image3 = slotThree.GetComponent<Image>();
+
+        return (image1.sprite.name == "slot-symbol4" && image2.sprite.name != "slot-symbol4" && image3.sprite.name != "slot-symbol4") ||
+               (image1.sprite.name != "slot-symbol4" && image2.sprite.name == "slot-symbol4" && image3.sprite.name != "slot-symbol4") ||
+               (image1.sprite.name != "slot-symbol4" && image2.sprite.name != "slot-symbol4" && image3.sprite.name == "slot-symbol4");
     }
 
 }
